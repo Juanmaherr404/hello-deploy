c95dcf2439fe389585a99fafcbb69a6f2a49f725
Juanmaherr404 <juanma.herrera.mer@gmail.com>
2025-05-07 17:30:17 +0200

    build

---
+++ server.js
@@ -0,0 +1,114 @@
+const passport = require("passport");
+const GithubStrategy = require("passport-github2").Strategy;
+const session = require("express-session");
+const express = require("express");
+const dotenv = require("dotenv").config();
+const app = express();
+const exec = require("child_process").exec;
+app.use(express.static("public"));
+const CLIENT_ID_GITHUB = process.env.CLIENT_ID_GITHUB;
+const CLIENT_SECRET_GITHUB = process.env.CLIENT_SECRET_GITHUB;
+const IP = process.env.IP || "localhost";
+console.log(`CLIENT_ID_GITHUB: ${CLIENT_ID_GITHUB}`);
+console.log(`CLIENT_SECRET_GITHUB: ${CLIENT_SECRET_GITHUB}`);
+console.log(`IP: ${IP}`);
+passport.use(new GithubStrategy({
+    clientID: CLIENT_ID_GITHUB,
+    clientSecret: CLIENT_SECRET_GITHUB,
+    callbackURL: `http://${IP}:3000/auth/github/callback`,
+}, function (accessToken, refreshToken, profile, done) {
+    console.log("Profile: ", profile);
+    return done(null, profile);
+}));
+
+passport.serializeUser((user, done) => {
+    console.log("Serializing user:", user);
+    done(null, user);
+});
+
+passport.deserializeUser((user, done) => {
+    console.log("Deserializing user:", user);
+    done(null, user);
+});
+
+app.use(session({
+    secret: "supersecreto",
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false } // Cambia a true si usas HTTPS
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Middleware para analizar el cuerpo de la solicitud
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const middlewareAuth = (req, res, next) => {
+    if (req.isAuthenticated()) {
+        console.log("User is authenticated");
+        return next();
+    } else {
+        console.log("User is not authenticated");
+        return res.redirect("/");
+    }
+}
+
+app.get("/auth/github", 
+    passport.authenticate('github', { scope: ['user:email'] 
+}));
+
+app.get("/auth/github/callback", passport.authenticate('github', { failureRedirect: '/' }),
+    (req, res) => {
+        console.log("User authenticated successfully");
+        res.redirect("/recon");
+    }
+);
+
+app.get("/profile", middlewareAuth, (req, res) => {
+    const html = `Hola ${req.user.username || req.user.displayName}`
+    res.send(html)
+})
+
+app.get("/recon", middlewareAuth, (req, res) => {
+    res.sendFile(__dirname + "/public/recon.html");
+})
+
+app.get("/run-command", middlewareAuth, (req, res) => {
+    exec("touch test", (error, stdout, stderr) => {
+        if (error) {
+            console.error(`exec error: ${error}`);
+            return res.status(500).send("Error executing script");
+        }
+        console.log(`stdout: ${stdout}`);
+        console.error(`stderr: ${stderr}`);
+        res.send("Script executed successfully");
+    });
+});
+
+app.post("/recon", middlewareAuth, (req, res) => {
+    console.log("Received request to /recon");
+    const domain = req.body.domain || req.query.domain;
+    const APIKEY = req.body.APIKEY || req.query.APIKEY;
+
+    console.log(`Domain: ${domain}`);
+    console.log(`APIKEY: ${APIKEY}`);
+    if(APIKEY !== "1234") {
+        return res.status(403).send("Forbidden");
+    } else {
+    exec(`./recon.sh ${domain} > resultados`, (error, stdout, stderr) => {
+        if (error) {
+            console.error(`exec error: ${error}`);
+            return res.status(500).send("Error executing script");
+        } else {
+            console.log(`stdout: ${stdout}`);
+            console.error(`stderr: ${stderr}`);
+            res.send("Script executed successfully");
+        } 
+    });
+    }
+});
+
+app.listen(3000, () => {
+    console.log("Server is running on port 3000");
+});
