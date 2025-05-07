95b8818307f585d2584ef49d27e2db4346039c6d
Juanmaherr404 <juanma.herrera.mer@gmail.com>
2025-05-07 17:59:37 +0200

    ACT

--- server.js
+++ server.js
@@ -110,5 +110,5 @@ app.post("/recon", middlewareAuth, (req, res) => {
 });
 
 app.listen(3000, () => {
-    console.log("Server is running on port 3000");
+    console.log("Server is running on port: 3000");
 });
